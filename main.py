77bf2c21f33f8073161b09cd28b1e57009f35b68
Aaron Tyler <a.tyler04@outlook.com>
2026-05-03 17:30:21 -0400

    commenting

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ dataset = SegDataset(
 
 model = None
 load_model = False
+model_name = 'best_model_untrained'
 weights = 'imagenet'
 
 if load_model:
-    model_name = 'best_model_untrained'
     DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
     model = torch.load(f'models/{model_name}.pt', map_location=DEVICE, weights_only=False)
 else:
@@ -33,5 +33,5 @@ seg_model = SegModel(EPOCHS=30, BATCH_SIZE=32, NUM_CLASSES=5,
                      model=model, dataset=dataset)
 
 # seg_model.train(train_split=0.15, val_split=0.15, saved_model_name='best_model_untrained')
-seg_model.test()
+# seg_model.test()
 seg_model.load_example(90)
